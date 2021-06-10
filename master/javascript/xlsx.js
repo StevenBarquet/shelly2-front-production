9d41a6f039fc586ef84b60e97de1c28d4845a678
Steven Barquet <mlmjm123@gmail.com>
2021-06-10 09:39:53 -0500

    some changes

--- master/javascript/xlsx.js
+++ master/javascript/xlsx.js
@@ -1,1 +1,1 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{697:function(n,o){},776:function(n,o){},777:function(n,o){}}]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{696:function(n,o){},775:function(n,o){},776:function(n,o){}}]);
