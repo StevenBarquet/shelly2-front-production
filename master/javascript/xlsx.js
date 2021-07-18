bed06bc3453b2328b4fa50f2ccc19e8a5b9ed511
Ansible root <mlmjm123@gmail.com>
2021-07-18 13:49:53 -0500

    app updates from ansible

--- master/javascript/xlsx.js
+++ master/javascript/xlsx.js
@@ -1,1 +1,1 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{697:function(n,o){},776:function(n,o){},777:function(n,o){}}]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{698:function(n,o){},777:function(n,o){},778:function(n,o){}}]);
